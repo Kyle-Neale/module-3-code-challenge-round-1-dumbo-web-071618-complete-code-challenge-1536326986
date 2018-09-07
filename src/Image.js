66f2606c51d8e75d7bc79e1f5f7289d16fc4819b
Kyle-Neale <kyle.a.neale@gmail.com>
2018-09-07 10:57:32 -0400

    Done

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,43 @@
-class Image {
-  
+class ImageAdapter {
 
+  static getImage(url) {
+    return fetch(url)
+    .then(resp => resp.json())
+    .then(data => data)
+  }
+
+  static updateImageLikes(id) {
+    return fetch("https://randopic.herokuapp.com/likes", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: id
+      })
+    })
+  }
+
+  static updateComments(id, commentContent) {
+    return fetch("https://randopic.herokuapp.com/comments", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: id,
+        content: commentContent
+      })
+    })
+    .then(resp => resp.json())
+    .then(data => data)
+  }
+
+  static deleteComment(id) {
+    return fetch(`https://randopic.herokuapp.com/comments/${id}`, {
+      method: "DELETE"
+    })
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imageId = 1 //Enter your assigned imageId here
-
+  const imageId = 79 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const imageTag = document.querySelector("#image");
+  const imageName = document.querySelector("#name");
+  const likes = document.querySelector("#likes");
+  const likeBttn = document.querySelector("#like_button");
+  const commentForm = document.querySelector("#comment_form");
+  const commentSection = document.querySelector("#comments");
 
+  ImageAdapter.getImage(imageURL)
+  .then(imageObj => {
+    imageTag.src = imageObj.url;
+    imageName.innerText = imageObj.name
+    likes.innerText = imageObj.like_count
+
+    imageObj.comments.forEach(comment => {
+      let deleteBttn = document.createElement("button")
+
+      let commentLi = document.createElement("li");
+      commentLi.innerText = comment.content;
+      commentLi.dataset.id = comment.id;
+      commentSection.appendChild(commentLi);
+      deleteBttn.innerText = "Delete Comment"
+      commentLi.appendChild(deleteBttn)
+
+    })
+  })
+
+  likeBttn.addEventListener("click", (event) => {
+    let likeCount = likes.innerText
+    let count = parseInt(likeCount);
+    ++count;
+    likes.innerText = count;
+    ImageAdapter.updateImageLikes(imageId);
+  })
+
+  commentForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let formValue = commentForm.querySelector("#comment_input");
+    ImageAdapter.updateComments(imageId, formValue.value)
+    .then(commentObj => {
+      let deleteBttn = document.createElement("button")
+      deleteBttn.innerText = "Delete Comment"
+
+      let commentLi = document.createElement("li");
+      commentLi.innerText = formValue.value;
+      commentLi.dataset.id = commentObj.id;
+      commentLi.appendChild(deleteBttn)
+      commentSection.appendChild(commentLi);
+
+      formValue.value = "";
+    })
+  })
+
+  commentSection.addEventListener("click", (event) => {
+    if (event.target.parentNode.hasAttribute("data-id")) {
+      let id = event.target.parentNode.dataset.id
+      ImageAdapter.deleteComment(id).then(data => {
+        event.target.parentNode.remove();
+      })
+    }
+  })
 
 })
-
